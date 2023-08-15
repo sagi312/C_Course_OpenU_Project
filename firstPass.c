63e8d38c8931930c8a8b9b219baba169ac4e823d
sagi <sagi@kushner.co.il>
2023-08-15 22:30:27 +0300

    Added get word count and moved type checking to another file

--- firstPass.c
+++ firstPass.c
@@ -5,6 +5,7 @@
 #include "table.h"
 #include "inputOutput.h"
 #include "config.h"
+#include "typeChecker.h"
 
 /*problems  - easy way to find patterns, regex?
             - complicated data handeling with tables, memory leaks
@@ -16,6 +17,7 @@
 firstPass(FILE* file, Table* symbolTable, Table* fileTable) {
     int ic = 0, dc = 0, i = 0, errorFlag = 0, labelFlag = 0;
     char *line;
+    InstructionType type;
     TokenLine *tokens;
     Table *codeSymbolTable, *dataSymbolTable, *codeTable, *dataTable, *opTable;
 
@@ -23,42 +25,48 @@ firstPass(FILE* file, Table* symbolTable, Table* fileTable) {
     while(line != NULL){
         tokens = tokenizeLine(line, i);
 
-        /*Skip comments*/
-        if(isComment(tokens))
-            continue;
-        /*Check if there's a label in the line. If there was an error with the label, still set label flag for op detectaion*/
+        /*Check if there's a label in the line. If there was an error with the label, still set label flag for rest of line detection*/
         if(hasLabel(tokens, codeSymbolTable, dataSymbolTable) != 0)
             labelFlag = 1;
 
+        type = getInstructType(tokens, codeSymbolTable, dataSymbolTable, labelFlag);
+
+        /*Skip comments*/
+        if(type == Comment)
+            continue;
+        
         /*Check if the line is a .data line*/
-        if(isData(tokens)){
+        if(type == Data){
             if(labelFlag)
                 addLabel(tokens, dataSymbolTable, dc);
             else
                 printWarning("Data declaration without a label", i);
-            dc += getWordCount(tokens);
+            dc += getWordCount(tokens, Data, labelFlag);
             addData(tokens, dataTable);
         }
+
         /*Check if the line is a .string line*/
-        else if(isString(tokens)){
+        else if(type == String){
             if(labelFlag)
                 addLabel(tokens, dataSymbolTable, dc);
             else
                 printWarning("String declaration without a label", i);
-            dc += getWordCount(tokens);
+            dc += getWordCount(tokens, String, labelFlag);
             addString(tokens, dataTable);
         }
+
         /*Check if the line is a .extern line*/
-        else if(isExtern(tokens)){
-                addExternLabels(tokens, symbolTable);
+        else if(type == Extern){
+            addExternLabels(tokens, symbolTable);
         }
-        else{ 
+
+        else if(type == Op) {
             if(labelFlag)
                 addLabel(tokens, codeSymbolTable, ic);
-            if(hasOp(tokens, opTable, labelFlag))
-                ic += getWordCount(tokens);
-                addOp(tokens, dataTable);
+            ic += getWordCount(tokens, Op, labelFlag);
+            addOp(tokens, dataTable);
         }
+
         i++;
         free(line);
         freeTokenLine(tokens);
@@ -67,108 +75,119 @@ firstPass(FILE* file, Table* symbolTable, Table* fileTable) {
     if(errorFlag)
         return -1;
     symbolTable = codeSymbolTable;
-    connectDataTable(symbolTable, dataSymbolTable, ic);
-    connectTables(codeTable, dataTable);
-}
-
-/*Check if a line is a comment*/
-isComment(TokenLine* tokens) {
-    char* firstField = getTokenField(0, tokens);
-    if(firstField == NULL)
-        return 0;
-    if(firstField[0] == ';') {
-        free(firstField);
-        return 1;
-    }
-    free(firstField);
+    connectDataTable(symbolTable, codeSymbolTable, dataSymbolTable, ic);
     return 0;
 }
 
-/*Check if a line has a label*/
-hasLabel(TokenLine* tokens, Table* codeSymbolTable, Table* dataSymbolTable){
-    char *name, *firstField = getTokenField(0, tokens);
-    int i, length;
+/*Get the number of words the data/code will take up in final file*/
+getWordCount(TokenLine* tokens, InstructionType type, int labelFlag) {
+    if(type == Op)
+        return getOpWordCount(tokens, labelFlag);
+    else if(type == Data)
+        return getDataWordCount(tokens, labelFlag);
+    else if(type == String)
+        return getStringWordCount(tokens, labelFlag);
+    else
+        return 0;
+}
 
-    if(firstField == NULL)
+getOpWordCount(TokenLine* tokens, int labelFlag){
+    Table *group1, *group2, *group3;
+    char *opName, *opParms, *parm1, *parm2, *group1Arr[] = OP_GROUP1, *group2Arr[] = OP_GROUP2, *group3Arr[] = OP_GROUP3;
+    int i, opCode, opWordCount;
+    if(labelFlag) {
+        opName = getTokenField(1, tokens);
+        opParms = getParmString(1, tokens);
+    }
+    else {
+        opName = getTokenField(0, tokens);
+        opParms = getParmString(0, tokens);
+    }
+
+    if(opName == NULL) {
+        return 0;
+    }
+    
+    group1 = createTable();
+    group2 = createTable();
+    group3 = createTable();
+
+    for(i = 0; i < OP_GROUP1_COUNT; i++)
+        addCell(group1Arr[i], group1);
+    for(i = 0; i < OP_GROUP2_COUNT; i++)
+        addCell(group2Arr[i], group2);
+    for(i = 0; i < OP_GROUP3_COUNT; i++)
+        addCell(group3Arr[i], group3);
+
+    if(inTable(opName, group1)) {
+        parm1 = strtok(opParms, ", \t");
+        parm2 = strtok(NULL, ", \t");
+        if(isRegister(parm1) && isRegister(parm2))
+            opWordCount = 2;
+        else
+            opWordCount = 3;
+    }
+    else if(inTable(opName, group2))
+        opWordCount = 2;
+    else
+        opWordCount = 1;
+
+    free(opName);
+    free(opParms);
+    freeTable(group1);
+    freeTable(group2);
+    freeTable(group3);
+    return opWordCount;
+}
+
+getDataWordCount(tokens, labelFlag) {
+    char* parmString;
+    int wordCount = 0;
+    if(labelFlag)
+        parmString = getParmString(1, tokens);
+    else
+        parmString = getParmString(0, tokens);
+    
+    if(parmString == NULL)
         return 0;
     
-    /*Check if the label is too short or too long*/
-    if(strlen(firstField) <= 1 || strlen(firstField) > MAX_LABEL_LENGTH) {
-        free(firstField);
-        return 0;
-    }
-
-    if(firstField[strlen(firstField) - 1] == ':') {
-        name = malloc(sizeof(char) * (strlen(firstField)-1));
-        strncpy(name, firstField, strlen(firstField)-1);
-        if(inTable(name, codeSymbolTable) || inTable(name, dataSymbolTable)){
-            printError("Label already exists", getLineNumber(tokens));
-            free(name);
-            free(firstField);
-            return -1;
-        }
-        if(!isalpha(name[0])) {
-            printError("Label must start with a letter", getLineNumber(tokens));
-            free(name);
-            free(firstField);
-            return -1;
-        }
-        length = strlen(name);
-        for(i = 0; i < length; i++) {
-            if(!isalnum(name[i])) {
-                printError("Label must contain only letters and numbers", getLineNumber(tokens));
-                free(name);
-                free(firstField);
-                return 0;
-            }
-            free(firstField);
-            return -1;
-        }
-    }
-    free(firstField);
-    return 0;
+    /*Initilize strtok*/
+    strtok(parmString, ", \t");
+    while(strtok(NULL, ", \t") != NULL)
+        wordCount++;
+    
+    free(parmString);
+    /*Add one for first number*/
+    return ++wordCount;
 }
 
-/*Check if a line has an operation*/
-hasOp(TokenLine* tokens, Table* opTable, int labelFlag){
-    char* opName;
+getStringWordCount(tokens, labelFlag) {
+    char* parmString;
+    int wordCount = 0;
     if(labelFlag)
-        opName = getTokenField(1, tokens);
+        parmString = getParmString(1, tokens);
     else
-        opName = getTokenField(0, tokens);
-    if(opName == NULL)
+        parmString = getParmString(0, tokens);
+    
+    if(parmString == NULL)
         return 0;
-    if(inTable(opName, opTable)) {
-        free(opName);
-        return 1;
-    }
-    free(opName);
-    return 0;
+    
+    /*1 word for each letter + 1 for /0 - 2 for "" */
+    wordCount = strlen(parmString) - 1;
+    free(parmString);
+    return wordCount;
 }
 
-/*Check if a line is a .data declaration of data*/
-isData(TokenLine* tokens){
-    return 0;
-}
-
-/*Check if a line is a .string declaration of data*/
-isString(TokenLine* tokens) {
-    return 0;
-}
-/*Check if the line has an extern declaration*/
-isExtern(TokenLine* tokens) {
-    return 0;
-}
-/*Get the number of words the data/code will take up in final file*/
-getWordCount(TokenLine* tokens) {
-    return 0;
-}
 /*Add data to the data table*/
 addData(TokenLine* tokens, Table* dataTable) {
     return 0;
 }
 /*Add string to the data table*/
 addString(TokenLine* tokens, Table* dataTable) {
+    return 0;
+}
+/*Add an opertaion to the codeTable (without the label. It will be marked as labelname_1 and changed afterwards)*/
+addOp(TokenLine* tokens, Table* codeTable) {
     return 0;
 }
 /*Add a label to the symbol table*/
@@ -179,11 +198,25 @@ addLabel(TokenLine* tokens, Table* symbolTable, int address) {
 addExternLabels(TokenLine* tokens, Table* symbolTable) {
     return 0;
 }
-/*Add an opertaion to the codeTable (without the label. It will be marked as labelname_1 and changed afterwards)*/
-addOp(TokenLine* tokens, Table* codeTable) {
+/*Connect the codeSymbolTable to the dataSymbolTable with the appropriate offsets*/
+connectDataTable(Table* symbolTable, Table* codeSymbolTable, Table* dataSymbolTable, int ic) {
     return 0;
 }
-/*Connect the codeSymbolTable to the dataSymbolTable with the appropriate offsets*/
-connectDataTable(Table* codeSymbolTable, Table* dataSymbolTable, int ic) {
-    return 0;
+
+/*Get the parameter string for .extern, .entry and .data lines*/
+char* getParmString(int lastField, TokenLine* tokens) {
+    char* parmString = getTokenField(++lastField, tokens), *nextField;
+
+    if(parmString == NULL)
+        return NULL;
+        
+    while((nextField = getTokenField(++lastField, tokens)) != NULL) {
+        parmString = realloc(parmString, sizeof(char) * (strlen(parmString) + strlen(nextField)));
+        if(parmString == NULL){
+            printError("Memory allocation failed", getLineNumber(tokens));
+            return NULL;
+        }
+        strcat(parmString, nextField);
+    }
+    return parmString;
 }
--- firstPass.h
+++ firstPass.h
@@ -3,22 +3,12 @@
 #include <ctype.h>
 #include "table.h"
 #include "inputOutput.h"
+#include "typeChecker.h"
 #include "config.h"
 
 int firstPass(FILE* file, Table* symbolTable, Table* fileTable);
-int isComment(TokenLine* tokens);
-/*Check if a line has a label*/
-int hasLabel(TokenLine* tokens, Table* codeSymbolTable, Table* dataSymbolTable);
-/*Check if a line has an operation*/
-int hasOp(TokenLine* tokens, Table* opTable, int labelFlag);
-/*Check if a line is a .data declaration of data*/
-int isData(TokenLine* tokens);
-/*Check if a line is a .string declaration of data*/
-int isString(TokenLine* tokens);
-/*Check if the line has an extern declaration*/
-int isExtern(TokenLine* tokens);
 /*Get the number of words the data/code will take up in final file*/
-int getWordCount(TokenLine* tokens);
+int getWordCount(TokenLine* tokens, InstructionType type, int labelFlag);
 /*Add data to the data table*/
 int addData(TokenLine* tokens, Table* dataTable);
 /*Add string to the data table*/
@@ -28,4 +18,5 @@ int addLabel(TokenLine* tokens, Table* symbolTable, int address);
 /*Add an opertaion to the codeTable (without the label. It will be marked as labelname_1 and changed afterwards)*/
 int addOp(TokenLine* tokens, Table* codeTable);
 /*Connect the codeSymbolTable to the dataSymbolTable with the appropriate offsets*/
-int connectDataTable(Table* codeSymbolTable, Table* dataSymbolTable, int ic);
+int connectDataTable(Table* symbolTable, Table* codeSymbolTable, Table* dataSymbolTable, int ic);
+char* getParmString( int lastField, TokenLine* tokens);
