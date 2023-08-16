b7ae627f20cc328cc6ea3c3b8f40f0b864eaf028
sagi <sagi@kushner.co.il>
2023-08-17 02:29:01 +0300

    Added encoding functions

--- firstPass.h
+++ firstPass.h
@@ -12,11 +12,13 @@ int getWordCount(TokenLine* tokens, InstructionType type, int labelFlag);
 /*Add data to the data table*/
 int addData(TokenLine* tokens, Table* dataTable, int labelFlag);
 /*Add string to the data table*/
-int addString(TokenLine* tokens, Table* dataTable);
+int addString(TokenLine* tokens, Table* dataTable, int labelFlag);
 /*Add a label to the symbol table*/
 int addLabel(TokenLine* tokens, Table* symbolTable, int address);
+addExternLabels(TokenLine* tokens, Table* symbolTable, int labelFlag);
 /*Add an opertaion to the codeTable (without the label. It will be marked as labelname_1 and changed afterwards)*/
-int addOp(TokenLine* tokens, Table* codeTable);
+int addOp(TokenLine* tokens, Table* codeTable, int labelFlag);
+int getAddresingType(char* parm, int lineNumber);
 /*Connect the codeSymbolTable to the dataSymbolTable with the appropriate offsets*/
 int connectDataTable(Table* symbolTable, Table* codeSymbolTable, Table* dataSymbolTable, int ic);
 char* getParmString( int lastField, TokenLine* tokens);
